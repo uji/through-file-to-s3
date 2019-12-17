c2ff5c0a5212934e34168d4a8a3d226a3f0c5527
ujiprog <ujiprog@gmail.com>
2019-12-18 01:24:40 +0900

    add comment linted golint -set_exit_status

--- main.go
+++ main.go
@@ -1,12 +1,17 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
+// SaveSoundRequest is struct for parse request body
 type SaveSoundRequest struct {
 	File string `json:"file"`
 }
@@ -24,6 +29,8 @@ func Handler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, e
 		return events.APIGatewayProxyResponse{}, err
 	}
 
+	uploadFile(request.File)
+
 	return events.APIGatewayProxyResponse{
 		StatusCode: 200,
 		Body:       request.File,
@@ -32,4 +39,18 @@ func Handler(r events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, e
 
 func main() {
 	lambda.Start(Handler)
+}
+
+func uploadFile(data string) (*s3manager.UploadOutput, error) {
+	sess := session.Must(session.NewSession(&aws.Config{
+		Region: aws.String("ap-northeast-1"),
+	}))
+	uploader := s3manager.NewUploader(sess)
+
+	return uploader.Upload(&s3manager.UploadInput{
+		ACL:    aws.String("public-read"),
+		Bucket: aws.String("test-bucket"),
+		Key:    aws.String("hoge.png"),
+		Body:   bytes.NewReader([]byte(data)),
+	})
 }
